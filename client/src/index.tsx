b5f0d0ea660c4e5e02c25c887ed0c0926f8655bd
Neoprot <kaua.seichi.gomes@gmail.com>
2024-12-06 21:53:29 -0300

    feat:Recriando organização de pastas para adicionar o tailwind

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'
+import './index.css';
 import App from './App';
-import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
