c65f584868db16926ef1f7ca88671b6f5d763c82
ArthurGuilher62 <arthurguilher062@gmail.com>
2024-12-10 21:15:16 -0300

    adicionando routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Gastos from './pages/Gastos/Gastos';
+import Home from './pages/Home/Home';
+import SobreNos from './pages/SobreNos/SobreNos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/SobreNos" element={<SobreNos />} />
+        <Route path="/Gastos" element={<Gastos />} />
+      </Routes>
+    </Router>
   );
 }
 
