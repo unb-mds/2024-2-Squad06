256f3369515da35db67b20b131c2d009bc54be10
TiagoTeixeira-2005 <jorgelucass1984@gmail.com>
2025-01-12 07:35:38 -0300

    integração do footer para tailwind e responsividade

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '860': '860px',
+      },
+    },
   },
   plugins: [],
 };
