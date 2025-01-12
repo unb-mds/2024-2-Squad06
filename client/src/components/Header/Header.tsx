34f7ce5468fcfba7f3ee43ea6b938fdf58c5bc0f
ArthurGuilher62 <arthurguilher062@gmail.com>
2025-01-12 09:54:46 -0300

    integração para tailwind e resposividade

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 function Header() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div>
-      <img src="/assets/unb.png" className="unb" alt="Unb" />
-      <header className="App-header">
-        <div className="logo-container">
-          <img src="/assets/logo.png" className="App-logo" alt="Logo do App" />
-          <div id="title">
-            <a href="/">Gastos Públicos</a>
+    <div className="bg-white">
+      <img src="/assets/unb.png" className="unb mx-auto" alt="Unb" />
+      <header className="flex justify-between items-center p-3 bg-blue-600 text-white">
+        <div className="logo-container flex items-center">
+          <img src="/assets/logo.png" className="App-logo " alt="Logo do App" />
+          <div id="title" className="ml-3 text-xl">
+            <a href="/" className="text-white hover:text-gray-300">Gastos Públicos</a>
           </div>
         </div>
-        <ul className="navigation">
-          <li><a className="navigation__link" href="/Gastos" target="" rel="noopener noreferrer">Gastos</a></li>
-          <li><a className="navigation__link" href="" target="" rel="noopener noreferrer">Monitoramento</a></li>
-          <li><a className="navigation__link" href="/SobreProjeto" target="" rel="noopener noreferrer">Sobre o projeto</a></li>
-          <li><a className="navigation__link" href="/SobreNos" target="" rel="noopener noreferrer">Sobre nós</a></li>
-        </ul>
+
+        <button className="lg:hidden p-2 text-white sidebar-toggle" onClick={toggleSidebar}>
+          ☰
+        </button>
+
+        <nav
+          className={`inset-0 bg-gray-800 bg-opacity-100 z-50 transform 
+          ${isSidebarOpen ? 'translate-x-0 w-48' : '-translate-x-full w-48'} 
+          transition-transform duration-300 lg:relative lg:flex lg:items-center lg:justify-center lg:bg-transparent`}
+        >
+          <ul className="flex flex-col text-xl lg:flex-row items-center justify-center space-y-5 lg:space-y-0 lg:space-x-6 w-auto">
+            <li><a className="text-white hover:text-gray-300 navigation__link whitespace-nowrap" href="/Gastos">Gastos</a></li>
+            <li><a className="text-white hover:text-gray-300 navigation__link whitespace-nowrap" href="/Monitoramento">Monitoramento</a></li>
+            <li><a className="text-white hover:text-gray-300 navigation__link whitespace-nowrap" href="/SobreProjeto">Sobre o projeto</a></li>
+            <li><a className="text-white hover:text-gray-300 navigation__link whitespace-nowrap" href="/SobreNos">Sobre nós</a></li>
+          </ul>
+
+          <button 
+            className="absolute left-0 text-white p-2 lg:hidden text-2xl"
+            onClick={toggleSidebar}
+          >
+            ☰
+          </button>
+        </nav>
       </header>
     </div>
   );
