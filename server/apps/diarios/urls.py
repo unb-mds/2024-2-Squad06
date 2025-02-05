78ae7c242a833b11b18582e2b271875beec91d36
Neoprot <kaua.seichi.gomes@gmail.com>
2025-02-05 20:22:29 -0300

    feat: Adicionar novas rotas para listar fornecedores e buscar diários por fornecedor por ID
    Co-authored-by: Ana-Luiza-SC <ssoares.analuiza@gmail.com>

--- server/apps/diarios/urls.py
+++ server/apps/diarios/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import DiariosPorFornecedorAPIView, RequisicaoAPIView, FiltragemView
+from .views import  DiariosPorFornecedorByIdAPIView, FornecedorByNameAPIView, FornecedoresListAPIView, RequisicaoAPIView, FiltragemView
 
 urlpatterns = [
     path("requisicao/", RequisicaoAPIView.as_view(), name="requisicao"),
     path('filtragem/', FiltragemView.as_view(), name='filtragem'),
-    path('diarios-fornecedor/', DiariosPorFornecedorAPIView.as_view(), name='diarios-fornecedor'),
+    path("fornecedores/", FornecedoresListAPIView.as_view(), name="fornecedores"),
+    path("fornecedor/", FornecedorByNameAPIView.as_view(), name="fornecedor-by-name"),
+    path("diarios-fornecedor/<int:id>/", DiariosPorFornecedorByIdAPIView.as_view(), name="diarios-fornecedor-by-id"),
 ]
+
