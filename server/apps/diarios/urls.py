4adf3841c9fa0d14565f243decb5a9d2e683fe2c
Neoprot <kaua.seichi.gomes@gmail.com>
2025-02-06 01:43:26 -0300

    feat: Atualizar rota de filtragem para 'get-diarios' e adicionar nova visualização de diários

--- server/apps/diarios/urls.py
+++ server/apps/diarios/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import  DiariosPorFornecedorByIdAPIView, FornecedorByNameAPIView, FornecedoresListAPIView, RequisicaoAPIView, FiltragemView
+from .views import  DiariosPorFornecedorByIdAPIView, FornecedorByNameAPIView, FornecedoresListAPIView, RequisicaoAPIView, DiariosView
 
 urlpatterns = [
     path("requisicao/", RequisicaoAPIView.as_view(), name="requisicao"),
-    path('filtragem/', FiltragemView.as_view(), name='filtragem'),
+    path('get-diarios/', DiariosView.as_view(), name='Diarios'),
     path("fornecedores/", FornecedoresListAPIView.as_view(), name="fornecedores"),
     path("fornecedor/", FornecedorByNameAPIView.as_view(), name="fornecedor-by-name"),
     path("diarios-fornecedor/<int:id>/", DiariosPorFornecedorByIdAPIView.as_view(), name="diarios-fornecedor-by-id"),
