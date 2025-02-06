554f473990bb320c0a0271cac24076cb476f9b75
Neoprot <kaua.seichi.gomes@gmail.com>
2025-02-06 11:52:08 -0300

    feat: Adicionar contagem de contratos e filtrar fornecedores na lista

--- server/apps/diarios/views.py
+++ server/apps/diarios/views.py
@@ -3,12 +3,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime, timedelta
 
+from django.http import JsonResponse
+from django.db.models.functions import Length
+from django.db.models import Count
+
 from .serializers import FornecedorSerializer
 from .models import Diario
 from .services import Controladores
-from django.http import JsonResponse
 from .models import Fornecedor, Diario
-from django.db.models.functions import Length
 
 
 class RequisicaoAPIView(APIView):
@@ -114,7 +116,10 @@ class DiariosView(APIView):
 
 class FornecedoresListAPIView(APIView):
     def get(self, request):
-        fornecedores = Fornecedor.objects.annotate(nome_length=Length('nome')).filter(nome_length__gt=5)
+        fornecedores = Fornecedor.objects.annotate(
+            nome_length=Length('nome'),
+            contract_count=Count('fornecedor')
+        ).filter(nome_length__gt=5).order_by('-contract_count')[:10]
         serializer = FornecedorSerializer(fornecedores, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -132,7 +137,7 @@ class FornecedorByNameAPIView(APIView):
             fornecedores = Fornecedor.objects.filter(nome=fornecedor_nome)
             ids = list(fornecedores.values_list('id', flat=True))
             return Response({"ids": ids}, status=status.HTTP_200_OK)
-
+        
 class DiariosPorFornecedorByIdAPIView(APIView):
     def post(self, request, id):
         try:
@@ -141,107 +146,59 @@ class DiariosPorFornecedorByIdAPIView(APIView):
             return Response({"error": "Fornecedor não encontrado."}, status=status.HTTP_404_NOT_FOUND)
         
         nome_body = request.data.get('nome')
+        data_assinatura_str = request.data.get('data_assinatura')
+        data_publicacao_str = request.data.get('data_publicacao')
+        
         if nome_body and nome_body != fornecedor.nome:
             return Response(
                 {"error": "O nome fornecido não corresponde ao fornecedor com o id informado."},
                 status=status.HTTP_400_BAD_REQUEST
             )
+        
         diarios = Diario.objects.filter(contratacoes__fornecedor=fornecedor).distinct()
         
+        if data_assinatura_str:
+            try:
+                data_assinatura_date = datetime.strptime(data_assinatura_str, "%Y-%m-%d").date()
+                diarios = diarios.filter(contratacoes__data_assinatura=data_assinatura_date)
+            except ValueError:
+                return Response({"error": "Data de assinatura inválida. Use o formato YYYY-MM-DD."},
+                                status=status.HTTP_400_BAD_REQUEST)
+        
+        if data_publicacao_str:
+            try:
+                data_publicacao_date = datetime.strptime(data_publicacao_str, "%Y-%m-%d").date()
+                diarios = diarios.filter(date=data_publicacao_date)
+            except ValueError:
+                return Response({"error": "Data de publicação inválida. Use o formato YYYY-MM-DD."},
+                                status=status.HTTP_400_BAD_REQUEST)
+        
         resultado = []
+        print(len(diarios))
         for diario in diarios:
             diario_data = {
                 'id': diario.id,
-                'data_publicacao': diario.date.strftime('%Y-%m-%d') if diario.date else None,
-                'url': diario.url,
-                'txt_url': diario.txt_url,
-                'excerpts': diario.excerpts,
-                'contratacoes': []
-            }
-            contratacoes = diario.contratacoes.all()
-            for contratacao in contratacoes:
-                contrato_data = {
-                    'id': contratacao.id,
-                    'valor_mensal': contratacao.valor_mensal,
-                    'valor_anual': contratacao.valor_anual,
-                    'data_assinatura': contratacao.data_assinatura.strftime('%Y-%m-%d') if contratacao.data_assinatura else None,
-                    'vigencia': contratacao.vigencia,
-                    'fornecedor': {
-                        'id': contratacao.fornecedor.id if contratacao.fornecedor else None,
-                        'nome': contratacao.fornecedor.nome if contratacao.fornecedor else None,
-                        'cnpj': contratacao.fornecedor.cnpj if contratacao.fornecedor else None,
-                    }
-                }
-                diario_data['contratacoes'].append(contrato_data)
-            resultado.append(diario_data)
-        
-        return Response(resultado, status=status.HTTP_200_OK)
-    
-class FiltragemView(APIView):
-    def get(self, request):
-        data_publicacao = request.GET.get('data_publicacao')
-        data_assinatura = request.GET.get('data_assinatura') 
-        valor_mensal = request.GET.get('valor_mensal')
-        valor_anual = request.GET.get('valor_anual')
-        try:
-            page = int(request.GET.get('page', 1))
-        except ValueError:
-            page = 1
-
-        qs = Diario.objects.all()
-
-        if data_publicacao:
-            try:
-                pub_date = datetime.strptime(data_publicacao, "%Y-%m-%d").date()
-                qs = qs.filter(date=pub_date)
-            except ValueError:
-                return JsonResponse({"error": "Data de publicação inválida. Use o formato YYYY-MM-DD."}, status=400)
-
-        if data_assinatura:
-            try:
-                ass_date = datetime.strptime(data_assinatura, "%Y-%m-%d").date()
-                qs = qs.filter(contratacoes__data_assinatura=ass_date)
-            except ValueError:
-                return JsonResponse({"error": "Data de assinatura inválida. Use o formato YYYY-MM-DD."}, status=400)
-
-        if valor_mensal:
-            try:
-                valor_mensal = float(valor_mensal)
-                qs = qs.filter(contratacoes__valor_mensal=valor_mensal)
-            except ValueError:
-                return JsonResponse({"error": "Valor mensal inválido."}, status=400)
-
-        if valor_anual:
-            try:
-                valor_anual = float(valor_anual)
-                qs = qs.filter(contratacoes__valor_anual=valor_anual)
-            except ValueError:
-                return JsonResponse({"error": "Valor anual inválido."}, status=400)
-        qs = qs.distinct().order_by('-id')
-
-        start = (page - 1) * 9
-        end = start + 9
-        diarios_paginados = qs[start:end]
-
-        resultado = []
-        for diario in diarios_paginados:
-            diario_data = {
-                'id': diario.id,
-                'data_publicacao': diario.date.strftime('%Y-%m-%d') if diario.date else None,
+                'data_publicacao': diario.date.strftime("%Y-%m-%d") if diario.date else None,
                 'url': diario.url,
                 'txt_url': diario.txt_url,
                 'excerpts': diario.excerpts,
                 'contratacoes': []
             }
             for contratacao in diario.contratacoes.all():
-                contrato_data = {
-                    'id': contratacao.id,
-                    'valor_mensal': contratacao.valor_mensal,
-                    'valor_anual': contratacao.valor_anual,
-                    'data_assinatura': contratacao.data_assinatura.strftime('%Y-%m-%d') if contratacao.data_assinatura else None,
-                    'vigencia': contratacao.vigencia,
-                }
-                diario_data['contratacoes'].append(contrato_data)
+                if len(contratacao.fornecedor.nome) > 6:
+                    contrato_data = {
+                        'id': contratacao.id,
+                        'valor_mensal': contratacao.valor_mensal,
+                        'valor_anual': contratacao.valor_anual,
+                        'data_assinatura': contratacao.data_assinatura.strftime("%Y-%m-%d") if contratacao.data_assinatura else None,
+                        'vigencia': contratacao.vigencia,
+                        'fornecedor': {
+                            'id': contratacao.fornecedor.id if contratacao.fornecedor else None,
+                            'nome': contratacao.fornecedor.nome if contratacao.fornecedor else None,
+                            'cnpj': contratacao.fornecedor.cnpj if contratacao.fornecedor else None,
+                        }
+                    }
+                    diario_data['contratacoes'].append(contrato_data)
             resultado.append(diario_data)
-
-        return JsonResponse(resultado, safe=False, status=200)
+        
+        return Response(resultado, status=status.HTTP_200_OK)
